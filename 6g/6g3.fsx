3717a74827c6d0f4d97ed54c8386620732a5ce81
David Rasmussen Lolck <derzeedk@gmail.com>
2018-10-23 15:43:39 +0200

    bugfixes in 6g3, and with better pattern matching

--- 6g/6g3.fsx
+++ 6g/6g3.fsx
@@ -1,21 +1,21 @@
+// Tager en liste af kædebrøker, samt et i fortællende hvor præcis en approximation
+// retur værdien. Dette vil være t_i/n_i, returneret som en tuple (t_i,n_i)
 let cfrac2frac (lst: int list) (i:int) : (int*int) = 
-    let rec f (lst: int list) (i:int) : (int*int) =
-        // For at undgå underflow fejl tilføjes 2 sentinel elementer til slutningen (bruges aldrig)
-        //printfn "%A %d" lst i
-        match i with
-        | -1 -> (1,0)
-        | -2 -> (0,1)
-        | _  ->
-            let (tim1,nim1) = f lst.Tail (i-1)
-            let (tim2,nim2) = f lst.Tail.Tail (i - 2)
-            let t = lst.Head * tim1 + tim2
-            let n = lst.Head * nim1 + nim2
+    let rec f (lst: int list) : (int*int) =
+        match lst with
+        | [] -> (1,0)
+        | h::[] -> (h,1)
+        | (h::m::t) ->
+            let (tim1,nim1) = f (m::t)
+            let (tim2,nim2) = f t
+            let t = h * tim1 + tim2
+            let n = h * nim1 + nim2
             (t,n)
     // Vender listen om og fjerner et antal elementer fra starten,
-    // i tilfælde af aproximationen ikke skal være fuldstændig.
-    let revLst = List.rev lst.[..i]
-    let s = revLst @ [0;0]
-    f s i
+    // i tilfælde af at aproximationen ikke skal være fuldstændig.
+    let revLst = List.rev (lst.[..i])
+    
+    f revLst
  
 let k = [3;4;12;4]
-printfn "%A" (cfrac2frac k 3)
+printfn "%A" (cfrac2frac k 0)
