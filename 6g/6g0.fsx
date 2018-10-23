7e984c6f9f9a506cfd9d1e472e3408550ad8259a
David Rasmussen Lolck <derzeedk@gmail.com>
2018-10-23 15:18:12 +0200

    Pattern matching in 6g0.fsx

--- 6g/6g0.fsx
+++ 6g/6g0.fsx
@@ -1,7 +1,6 @@
 let rec cfrac2float lst =
-  if List.isEmpty (List.tail lst) then
-    float (List.head lst)
-  else
-    float (List.head lst) +  (1.0 / cfrac2float (List.tail lst))
-
+  match lst with
+    | h::[] -> float h
+    | h::t ->  float h +  (1.0 / cfrac2float t)
+    | _ -> 0.0 // Dette er ikke en valid struktur, og er kun tilstede for at gøre compileren glad
 printfn "%f" (cfrac2float [0;3;4;12;3;1])
