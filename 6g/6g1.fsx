068c25ab108d1c13ff23e24e9ab926ff5b600497
GHanehoj <gustav.hanehoj@gmail.com>
2018-10-23 15:31:38 +0200

    Kommentarer til 6g1

--- 6g/6g1.fsx
+++ 6g/6g1.fsx
@@ -1,3 +1,11 @@
+(*
+    For at konvertere et reelt tal til en kædebrøk bruger vi algoritmen beskrevet
+    i opgavearket. Hver iteration findes heltalskomponenten af x, 'q', samt resten, 'rest'.
+    Hvis resten er 0 er algoritmen færdig, og det bagerste element skal være q-værdien
+    fra den iteration. Ellers kaldes float2cfrac igen, og den nuværende q-værdi sættes foran resultatet.
+*)
+
+// Tager en float og returnerer en liste af ints, som repræsenterer kædebrøken
 let rec float2cfrac (x:float) : int list =
     let q = int(x+10e-6)
     let rest = x-float(q)
@@ -5,5 +13,8 @@ let rec float2cfrac (x:float) : int list =
     | x when x < 10e-6 -> [q]
     | _ -> q::float2cfrac(1.0/rest)
 
+
+// For at teste at funktionen virker, bruger vi den på tallet 3.245, da dette burde returnere
+// [3;4;12;4] - hvilket vi også ser den gør
 let k = 3.245
 printfn "%A" (float2cfrac k)
