f31f4f94d0faf7a0041a36ecacd8a631796d20f1
David Rasmussen Lolck <derzeedk@gmail.com>
2018-10-23 13:13:42 +0200

    Cleaned 6g3, and made some bugfixes

--- 6g/6g3.fsx
+++ 6g/6g3.fsx
@@ -1,19 +1,22 @@
 let cfrac2frac (lst: int list) (i:int) : (int*int) = 
     let rec f (lst: int list) (i:int) : (int*int) =
         // For at undgå underflow fejl tilføjes 2 sentinel elementer til slutningen (bruges aldrig)
-        let sentinelLst = lst @ [0;0]
+        //printfn "%A %d" lst i
         if i = -1 then
             (1,0)
         elif i = -2 then
             (0,1)
         else
-            let t = sentinelLst.Head*(fst (f sentinelLst.Tail (i-1))) + fst((f sentinelLst.Tail.Tail (i-2)))
-            let n = sentinelLst.Head*(snd(f sentinelLst.Tail (i-1))) + (snd(f sentinelLst.Tail.Tail (i-2)))
+            let (tim1,nim1) = f lst.Tail (i-1)
+            let (tim2,nim2) = f lst.Tail.Tail (i - 2)
+            let t = lst.Head * tim1 + tim2
+            let n = lst.Head * nim1 + nim2
             (t,n)
     // Vender listen om og fjerner et antal elementer fra starten,
     // i tilfælde af aproximationen ikke skal være fuldstændig.
-    let revLst = (List.rev lst).[lst.Length-(i+1)..]
-    f revLst i
+    let revLst = List.rev lst.[..i]
+    let s = revLst @ [0;0]
+    f s i
  
 let k = [3;4;12;4]
-printfn "%A" (cfrac2frac k 3)
+printfn "%A" (cfrac2frac k 2)
