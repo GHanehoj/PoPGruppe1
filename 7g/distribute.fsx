8697d010b47ae92d8aaf7a46d1b4cea8a4464f9f
David Rasmussen Lolck <derzeedk@gmail.com>
2018-11-02 11:57:50 +0100

    Renamed for better testing of file. Implemented distrubute, but missing removal of old beans

--- 7g/distribute.fsx
+++ 7g/distribute.fsx
@@ -1,17 +1,33 @@
-module Awari
+//module Awari
+
+type player = Player1 | Player2
+type pit = {
+  index : int
+  beanCount: int
+}
+type board = pit list
+
 let boardSize = 14
 let distribute (b:board) (p:player) (i:pit) : (board*player*pit) =
     if i.beanCount = 0 then (b,p,i)
     else
-        let addBean (cp:pit) (index:int) : pit =
+        let addBean (index:int) (cp:pit) : pit =
             let indexDiff = (cp.index - i.index - 1 + boardSize) % boardSize
+            printfn "%i %i" indexDiff i.beanCount 
             let beanSum = cp.beanCount + i.beanCount / 14 // Should the chosen pit hold more than 14 beans
-            if diff < i.beanCount then
+            if indexDiff < i.beanCount then
                 {cp with beanCount = beanSum + 1}
             else
                 {cp with beanCount = beanSum}
         let lastIndex = (i.index + i.beanCount) % boardSize 
         let np = if lastIndex < boardSize / 2 then Player1 else Player2 
-        let ni = board.[lastIndex]
-        let nb = List.mapi addBean board
+        let ni = b.[lastIndex]
+        let nb = List.mapi addBean b
         (nb,np,ni)
+
+let b = List.init 14 (fun x -> {index = x; beanCount = 3})
+let p = Player1
+let i = b.[1]
+
+let (nb,np,ni) =  distribute b p i
+printfn "%A" nb
