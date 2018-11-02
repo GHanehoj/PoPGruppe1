35bbb7d168455804061428142c0e23c82095bdb5
GHanehoj <gustav.hanehoj@gmail.com>
2018-11-02 10:58:52 +0100

    Removing old code, putting functions into awariLib

--- 7g/awariLib.fs
+++ 7g/awariLib.fs
@@ -1,10 +1,33 @@
 module Awari
+open System
 type player = Player1 | Player2
 type pit = {
   index : int
   beanCount: int
 }
-type board = pit list
+type board = pit list 
+
+let isHome (b:board) (p:player) (i:pit) : bool = 
+    let pitPlayer = if i.index < 7 then Player1 else Player2
+    (pitPlayer = p) && (i.index%7=6)
+
+let isGameOver (b:board) : Boolean =
+  let mutable player1Beans = 0
+  let mutable player2Beans = 0
+  for pit in b do
+    if pit.index%7<>6 then
+      if pit.index >6 then player1Beans <- player1Beans + pit.beanCount
+      if pit.index <6 then player2Beans <- player2Beans + pit.beanCount
+  (player1Beans = 0) || (player2Beans = 0)
+
+let getMove (b:board) (p:player) (s:string) : pit = 
+  printfn "%s" s
+  let mutable inputPit = int (System.Console.ReadLine ())
+  while not (List.contains inputPit [1;2;3;4;5;6]) do
+    printfn "Incorrect input, try again with one of the pits 1-6"
+  match p with
+  | Player1 -> b.[inputPit-1]
+  | Player2 -> b.[inputPit+6]
 
 
 let turn (b : board) (p : player) : board =
--- 7g/isGameOver.fs
+++ 7g/isGameOver.fs
@@ -1,9 +1,17 @@
-module Awari
+module A
+open System
+type player = Player1 | Player2
+type pit = {
+  index : int
+  beanCount: int
+}
+type board = pit list
+
 let isGameOver (b:board) : Boolean =
   let mutable player1Beans = 0
   let mutable player2Beans = 0
-  for pit in b.pits do
-    if not pit.homePit then
-      if pit.side = Player1 then player1Beans <- player1Beans + pit.beanCount
-      if pit.side = Player2 then player2Beans <- player2Beans + pit.beanCount
+  for pit in b do
+    if pit.index%7<>6 then
+      if pit.index >6 then player1Beans <- player1Beans + pit.beanCount
+      if pit.index <6 then player2Beans <- player2Beans + pit.beanCount
   (player1Beans = 0) || (player2Beans = 0)
