d3997e70721f94cb552d0b274c0e1c189ea14b6c
David Rasmussen Lolck <derzeedk@gmail.com>
2018-11-19 10:41:41 +0100

    Finished distribute

--- 7g/distribute.fsx
+++ 7g/distribute.fsx
@@ -1,33 +1,62 @@
 //module Awari
-
 type player = Player1 | Player2
 type pit = {
   index : int
   beanCount: int
 }
 type board = pit list
+let newPlayer (p:player) :player = 
+    match p with
+    |Player1 -> Player2
+    |Player2 -> Player1
+
+ 
 
 let boardSize = 14
-let distribute (b:board) (p:player) (i:pit) : (board*player*pit) =
-    if i.beanCount = 0 then (b,p,i)
+
+let replaceAtIndex (index:int) (ni:pit) (b:board) : board =
+    let rep (currentIndex:int) (i:pit) =
+        if currentIndex = index then ni else i;
+    List.mapi rep b
+
+let getHome (b:board) (p:player):pit =
+    match p with
+    |Player1 -> b.[6]
+    |Player2 -> b.[13]
+
+let addBean (op:pit) (cp:pit) : pit =
+    let indexDiff = (cp.index - op.index - 1 + boardSize) % boardSize
+    // Should the chosen pit hold more than 14 beans
+    let beanSum = (if cp <> op then cp.beanCount else 0) + op.beanCount / boardSize 
+    if indexDiff < op.beanCount then
+        {cp with beanCount = beanSum + 1}
     else
-        let addBean (index:int) (cp:pit) : pit =
-            let indexDiff = (cp.index - i.index - 1 + boardSize) % boardSize
-            printfn "%i %i" indexDiff i.beanCount 
-            let beanSum = cp.beanCount + i.beanCount / 14 // Should the chosen pit hold more than 14 beans
-            if indexDiff < i.beanCount then
-                {cp with beanCount = beanSum + 1}
-            else
-                {cp with beanCount = beanSum}
+        {cp with beanCount = beanSum}
+
+let updateLastPit (b:board) (p:player) (i:pit) : (board*pit) = 
+    if i.beanCount = 1 then
+        let home = getHome b p
+        let oppositePit = b.[12 - i.index]
+        let updatedBoard =
+            b 
+            |> (replaceAtIndex home.index {home with beanCount = home.beanCount + i.beanCount + oppositePit.beanCount })
+            |> (replaceAtIndex (oppositePit.index) {oppositePit with beanCount = 0}) 
+            |> (replaceAtIndex i.index {i with beanCount = 0}) 
+        (updatedBoard,i) 
+    else
+       (b,i) 
+
+let distribute (b:board) (p:player) (i:pit) : (board*pit) =
+    if i.beanCount = 0 then (b,i)
+    else
         let lastIndex = (i.index + i.beanCount) % boardSize 
-        let np = if lastIndex < boardSize / 2 then Player1 else Player2 
         let ni = b.[lastIndex]
-        let nb = List.mapi addBean b
-        (nb,np,ni)
+        let nb = List.map (addBean ni) b
+        updateLastPit nb p ni
 
 let b = List.init 14 (fun x -> {index = x; beanCount = 3})
 let p = Player1
 let i = b.[1]
 
-let (nb,np,ni) =  distribute b p i
+let (nb,ni) =  distribute b p i
 printfn "%A" nb
