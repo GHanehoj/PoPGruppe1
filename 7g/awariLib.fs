cafc42389320ec4e08efdc1760c67110c0bda260
David Rasmussen Lolck <derzeedk@gmail.com>
2018-11-20 18:16:04 +0100

    Broke a couple of lines for easier latex formatting

--- 7g/awariLib.fs
+++ 7g/awariLib.fs
@@ -17,12 +17,14 @@ let printBoard (b:board) : unit =
     let homePitString = sprintf "%i%21i" b.[13].beanCount b.[6].beanCount 
     printfn "%s\n%s\n%s\n" player2String homePitString player1String
 
-// Hjælpefunktion til at bestemme om et bestemt felt er en bestemt spillers hjemmefelt
+// Hjælpefunktion til at bestemme om et 
+// bestemt felt er en bestemt spillers hjemmefelt
 let isHome (p:player) (i:pit) : bool = 
     let pitPlayer = if i.index < 7 then Player1 else Player2
     (pitPlayer = p) && (i.index%7=6)
 
-// Hjælpefunktion til at bestemme om en brætkonfiguration medfører at spillet er slut
+// Hjælpefunktion til at bestemme om en brætkonfiguration 
+// medfører at spillet er slut
 let isGameOver (b:board) : bool =
   let count (x:int,y:int) i:pit : int*int =
     if i.index%7<>6 then
@@ -34,7 +36,8 @@ let isGameOver (b:board) : bool =
   (p1Beans = 0 || p2Beans = 0)
 
 
-// Hjæplefunktion til at erstatte feltet i et bestemt index med et givent felt.
+// Hjæplefunktion til at erstatte feltet 
+// i et bestemt index med et givent felt.
 let replaceAtIndex (index:int) (ni:pit) (b:board) : board =
     let rep (currentIndex:int) (i:pit) =
         if currentIndex = index then ni else i;
@@ -46,36 +49,43 @@ let getHome (b:board) (p:player):pit =
     |Player1 -> b.[6]
     |Player2 -> b.[13]
 
-// Opdaterer det sidste felt, hvis dette felt kun indeholder 1 bønne, dvs. distribute endte i et tomt felt
+// Opdaterer det sidste felt, hvis dette felt kun 
+// indeholder 1 bønne, dvs. distribute endte i et tomt felt
 // ellers returnerers blot den givne konfiguration af felter 
 let updateLastPit (b:board) (p:player) (i:pit) : (board*pit) = 
     if i.beanCount = 1 && i.index % 7 <> 6 then
         let home = getHome b p
         let oppositePit = b.[12 - i.index] 
+        let newHomePitCount = home.beanCount + i.beanCount + oppositePit.beanCount 
         let updatedBoard =
             b 
-            |> (replaceAtIndex home.index {home with beanCount = home.beanCount + i.beanCount + oppositePit.beanCount })
-            |> (replaceAtIndex (oppositePit.index) {oppositePit with beanCount = 0}) 
-            |> (replaceAtIndex i.index {i with beanCount = 0}) 
+            |> replaceAtIndex home.index {home with beanCount = newHomePitCount}
+            |> replaceAtIndex oppositePit.index {oppositePit with beanCount = 0} 
+            |> replaceAtIndex i.index {i with beanCount = 0} 
         (updatedBoard,i) 
     else
        (b,i) 
 
 
-// Hovedfunktionen til at uddele bønnerne på et specifikt felt ud til alle de efterfølgende felter, 
+// Hovedfunktionen til at uddele bønnerne på et 
+// specifikt felt ud til alle de efterfølgende felter, 
 // samt fjerne de oprindelige bønder i startfeltet.
 let distribute (b:board) (p:player) (i:pit) : (board*pit) =
     
-    // Hjælpefunktion til at bestemme hvordan bønnerne i et givent felt cp ændre sig efter
+    // Hjælpefunktion til at bestemme hvordan 
+    // bønnerne i et givent felt cp ændre sig efter
     // bønnerne i "op" er blevet fordelt.
     // op er den pit som alle bønnerne kommer fra.
     let addBean (op:pit) (cp:pit) : pit =
         let indexDiff = (cp.index - op.index - 1 + boardSize) % boardSize
         
-        // Skulle det valgte felt indeholde nok bønder til at komme hele vejen rundt
-        // adderes dette tal til alle felter.
+        // Skulle det valgte felt indeholde nok bønder 
+        // til at komme hele vejen rundt adderes 
+        // dette tal til alle felter.
+        // 
         // Herudover sættes op til nu at have 0 bønner
-        let beanSum = (if cp <> op then cp.beanCount else 0) + op.beanCount / boardSize
+        let rounds = op.beanCount / boardSize
+        let beanSum = (if cp <> op then cp.beanCount else 0) + rounds
         
         // Hvis der var nok bønner i op til at nå det nuværrende felt,
         // forøges antallet af bønner her med 1.
@@ -88,8 +98,8 @@ let distribute (b:board) (p:player) (i:pit) : (board*pit) =
     else
         let lastIndex = (i.index + i.beanCount) % boardSize 
         let nb = List.map (addBean i) b
-        let ni = nb.[lastIndex]
-        updateLastPit nb p ni
+    let ni = nb.[lastIndex]
+    updateLastPit nb p ni
 
  
 let rec getAiMove (b:board) (p:player) (cIndex:int) (cMax:int) (maxIndex:int)= 
