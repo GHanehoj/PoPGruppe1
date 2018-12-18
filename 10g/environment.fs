f75b42123d8b2c24556a438023295140b491a1bb
GHanehoj <gustav.hanehoj@gmail.com>
2018-12-18 16:03:51 +0100

    Board changes

--- 10g/environment.fs
+++ 10g/environment.fs
@@ -1,1 +1,8 @@
 module Environment
+open AnimalFactory
+open Board
+
+type Environment(boardSize : int*int, wolfRepTime:int, wolfFeedTime:int, startWolfCount:int, mooseRepTime:int, startMooseCount:int) =
+    let board = Board()
+    let aFac = AnimalFactory(wolfRepTime, wolfFeedTime, mooseRepTime, board)
+    aFac.createRandom(startWolfCount, startMooseCount)
