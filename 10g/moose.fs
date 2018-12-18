1842df798e147e2bc2bd255a4bd747f390701d49
GHanehoj <gustav.hanehoj@gmail.com>
2018-12-18 15:19:03 +0100

    test

--- 10g/moose.fs
+++ 10g/moose.fs
@@ -1,22 +1,4 @@
 module Moose
-type AnimalFactory = int
-type position = int*int
-
-type action = Move of position
-             |Reproduce of position
-             |Eat of position
-
-[<AbstractClass>]
-type Animal(pos:position, repTime:int, aFac:AnimalFactory) =
-    let mutable _pos = pos
-    member this.pos with get() = _pos
-    member this.pos with set(newPos) = _pos <- newPos
-    member this.takeTurn() =
-    member this.tick
-
-
-
-
 
 type Moose(pos:position, repTime:int, aFac:AnimalFactory) =
     inherit Animal(pos,repTime, aFac)
