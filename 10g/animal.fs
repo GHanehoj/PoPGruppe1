2987620bbb915029033fbabea8a9e12a06e82e59
GHanehoj <gustav.hanehoj@gmail.com>
2018-12-20 13:42:21 +0100

    Added most of the Animal class and a general structure for environment.startSimulation

--- 10g/animal.fs
+++ 10g/animal.fs
@@ -1,5 +1,7 @@
 module Animal
 open AnimalFactory
+open Board
+open Microsoft.FSharp.Reflection
 type position = int*int
 
 type action = Move of position
@@ -7,9 +9,46 @@ type action = Move of position
              |Eat of position
 
 [<AbstractClass>]
-type Animal(pos:position, repTime:int, aFac:AnimalFactory) =
+type Animal(pos:position, repTime:int, aFac:AnimalFactory, brd:Board) =
     let mutable _pos = pos
-    member this.pos with get() = _pos
-    member this.pos with set(newPos) = _pos <- newPos
-    member this.takeTurn()
-    member this.tick
+    let generateMoves ((x,y) : position) =
+        let actionTypes = FSharpType.GetUnionCases typeof<action>
+        seq {
+            for actionType in actionTypes do
+                for i=(-1) to 1 do
+                    for j=(-1) to 1 do
+                        // We intentionally include actions to the same position
+                        // and will filter the invalid ones later.
+                        yield match actionType.Name with
+                              |"Move" -> Move(x+i, y+j)
+                              |"Reproduce" -> Reproduce(x+i, y+j)
+                              |_ -> Eat(x+i, y+j)
+        }  
+
+    let filterInvalidActions (actSeq: action seq) =
+        seq {
+            for act in actSeq do
+                if brd.findAtCoordinate(x,y) = None then
+                    match act with
+                    |Move(x,y) -> yield Move(x,y)
+                    |Reproduce(x,y) -> yield Reproduce(x,y)
+                if brd.findAtCoordinate(x,y) = Moose then
+                    match act with
+                    |Eat(x,y)  -> yield Eat(x,y)
+        }
+
+
+    member this.pos 
+        with get() = _pos
+        and set(newPos) = _pos <- newPos
+
+
+    abstract member prioritize : action seq -> action seq
+
+    member this.takeTurn() =
+        let availableActions = generateMoves(this.pos)
+        let legalActions = filterInvalidActions(availableActions)
+        let prioritizedActions = this.prioritize(legalActions)
+        // Execute (prioritizedActions.nth 1)
+        ()
+
--- 10g/environment.fs
+++ 10g/environment.fs
@@ -6,3 +6,10 @@ type Environment(boardSize : int*int, wolfRepTime:int, wolfFeedTime:int, startWo
     let board = Board()
     let aFac = AnimalFactory(wolfRepTime, wolfFeedTime, mooseRepTime, board)
     do aFac.createRandom(startWolfCount, startMooseCount)
+    member startSimulation() =
+        //1. Get random seq of all animals
+
+        //2. Tick each animal
+
+        //3. Print board
+        printfn "%A" board
