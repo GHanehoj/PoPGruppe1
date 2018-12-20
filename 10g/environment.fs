db0400cc8d2b17987668f2a27e60ead8cbfbcd03
GHanehoj <gustav.hanehoj@gmail.com>
2018-12-20 10:51:44 +0100

    asd

--- 10g/environment.fs
+++ 10g/environment.fs
@@ -5,4 +5,4 @@ open Board
 type Environment(boardSize : int*int, wolfRepTime:int, wolfFeedTime:int, startWolfCount:int, mooseRepTime:int, startMooseCount:int) =
     let board = Board()
     let aFac = AnimalFactory(wolfRepTime, wolfFeedTime, mooseRepTime, board)
-    aFac.createRandom(startWolfCount, startMooseCount)
+    do aFac.createRandom(startWolfCount, startMooseCount)
