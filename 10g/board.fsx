f6a80566686d8ea0b499f3773941e42b56f0df0c
David Rasmussen Lolck <derzeedk@gmail.com>
2018-12-20 18:18:37 +0100

    The board can now be used to shuffle the elements, deleting an element and inserting an element

---
+++ 10g/board.fsx
@@ -0,0 +1,13 @@
+//module Board
+open System
+type Board<'a when 'a : equality >() =
+    let mutable _lst : 'a list = []
+    member this.deleteThis (element:'a) = 
+        _lst <- List.filter (fun x -> x <> element) _lst
+    member this.getMembers () =
+        let rnd = Random()
+        // As there is no good shuffle of list in the standarb library
+        // This function simply sorts with a random key for each element
+        List.sortBy (fun _ -> rnd.Next()) _lst
+    member this.insert (element:'a) = 
+        _lst <- element::_lst
