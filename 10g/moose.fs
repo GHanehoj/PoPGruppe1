e742fdd3f11d10a4e5a8a62b8605f341626e358d
J.Johansen <jann.johansen1@gmail.com>
2018-12-27 19:02:25 +0100

    implemented nearbyWolves()

--- 10g/moose.fs
+++ 10g/moose.fs
@@ -3,6 +3,23 @@ module Moose
 type Moose(pos:position, repTime:int) =
     inherit Animal(pos,repTime)
 
+    let viewLength = 3
+    
+    // Returns sequence of position of wolves that are within view of the 
+    // given position. 
+    // (Skal muligvis boxe brd.findAtCoordinate(x+i, y+i) for ikke at få error) 
+    // Kan denne elegant gøres til en abstrakt klasse? 
+    let nearbyWolves ((x,y) : position) = 
+        seq {
+            for i = (-viewLength) to viewLength do 
+                for j = (-viewLength) to viewLength do 
+                    match brd.findAtCoordinate(x+i, y+i) with
+                    | :? Moose -> ()
+                    | _ -> yield (x+i, y+i) 
+        }
+
+
+
     override prioritize (actSeq: action seq) =
         ()
 
