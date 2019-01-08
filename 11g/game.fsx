0c63695485e312bc1d6131e213d45575249d59f7
David Rasmussen Lolck <derzeedk@gmail.com>
2019-01-08 21:28:10 +0100

    Added comments of game

--- 11g/game.fsx
+++ 11g/game.fsx
@@ -26,31 +26,37 @@ type Game () =
                 currentPlayer <- p2
             else 
                 currentPlayer <- p1
-
+    // Creates a starting chess board, with the correct configuration of the pieces.
     member private this.init () =
-        let brd = Board () 
+        let brd = Board ()
+        // Function to hel creating both a black and a white of a piece
         let mirror (x,y) = [(White,x,y);(Black,7-x,y)]
+        // Creates two black and two white of a piece
         let doubleMirror (x,y) =
             [(White,x,y); (White,x,7-y); (Black,7-x,y); (Black,7-x,7-y)]
+        // Creates a function that iis used in function composition to upcast the constructor from
+        // (Color -> rook/knight/bishop/king/queen) to (Color -> chessPiece) 
         let cons (c : chessPiece) = c
         let pieces = [
-            (knight >> cons,doubleMirror (0,1));
             (rook >> cons, doubleMirror (0,0));
+            (knight >> cons,doubleMirror (0,1));
             (bishop >> cons,doubleMirror (0,2));
             (king >> cons,mirror (0,3));
             (queen >> cons, mirror (0,4))
         ]
+        // function that given a constructor and a list of placements, creates all the pieces.
         let create (f,lst) =
             let it (c,x,y) =
                 brd.[x,y] <- Some (f c)
             List.iter it lst
         do List.iter create pieces
         brd
+    // Call this to start the game
     member this.start () = 
         let brd = this.init ()
         let gameMode =
             printfn "1: Player vs Player"
-            printfn "2: Player vs Computer"
+            printfn "2: Player vs Computer (Not implemented)"
             printf "Choose Gamemode: "  
             System.Console.ReadLine()
         
@@ -58,6 +64,7 @@ type Game () =
             let p1 = Human(brd, White)
             let p2 = Human(brd, Black) 
             run (brd, p1, p2)
+        // An AI is not implemented yet
         elif gameMode = "2" then
             let p1 = Human(brd, White)
             let p2 = Human(brd, Black)
