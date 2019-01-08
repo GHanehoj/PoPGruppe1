fbab21dc742af7452be43e8427154739672e25e8
David Rasmussen Lolck <derzeedk@gmail.com>
2019-01-08 15:03:07 +0100

    Added init of game
    The game now creates all the advanced pieces of the game, at the correct
    positions

--- 11g/game.fsx
+++ 11g/game.fsx
@@ -2,7 +2,7 @@ module Game
 #load "player.fsx"
 open Chess
 open Player
-
+open Pieces
 type Game () = 
     let run (brd : Board, p1 : Player, p2 : Player) =
         let mutable gameOver = false 
@@ -29,15 +29,33 @@ type Game () =
             else 
                 currentPlayer <- p1
 
-    member this.start () = 
+    member private this.init () =
         let brd = Board () 
-
+        let mirror (x,y) = [(White,x,y);(Black,7-x,y)]
+        let doubleMirror (x,y) =
+            [(White,x,y); (White,x,7-y); (Black,7-x,y); (Black,7-x,7-y)]
+        let cons (c : chessPiece) = c
+        let pieces = [
+            (knight >> cons,doubleMirror (0,1));
+            (rook >> cons, doubleMirror (0,0));
+            (bishop >> cons,doubleMirror (0,2));
+            (king >> cons,mirror (0,3));
+            (queen >> cons, mirror (0,4))
+        ]
+        let create (f,lst) =
+            let it (c,x,y) =
+                brd.[x,y] <- Some (f c)
+            List.iter it lst
+        do List.iter create pieces
+        brd
+    member this.start () = 
+        let brd = this.init ()
         let gameMode =
             printfn "1: Player vs Player"
             printfn "2: Player vs Computer"
             printf "Choose Gamemode: "  
             System.Console.ReadLine()
-
+        
         if gameMode = "1" then 
             let p1 = Human(brd, White)
             let p2 = Human(brd, Black) 
